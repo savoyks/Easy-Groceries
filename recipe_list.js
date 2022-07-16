75b10d90514115daf97909e9827d88a8c158ded6
savoyks <109036095+savoyks@users.noreply.github.com>
2022-07-15 22:02:21 -0400

    Added Recipes

--- recipe_list.js
+++ recipe_list.js
@@ -1,3 +1,104 @@
-const recipes= [
-    "Chicken", "Egg Rice", "Salmon", "Fried Chicken", "Peach and Tomato Salad "
-]
+const recipes = [
+    {
+        name: "Peachy Fattoush",
+        ingredients: [
+            "lemon", "agave nector", "red wine vinegar", "olive oil", "basil", "mint", "parsley", "peach", "grape tomatoes", "sumac"
+        ],
+        source: "Eden Grinshpan"
+    },
+    {
+        name: "Weeknight Salmon",
+        ingredients: ["salmon", "baby potatoes", "olive oil", "lemon", "pepper", "parsley", "butter", "paprika"],
+        source: "Joanna Gaines"
+    },
+    {
+        name: "Chickpea Masala",
+        ingredients: ["butter", "onion", "garlic", "ginger", "cumin", "coriander", "tumeric", "paprika", "san marzano tomatoes", "chickpeas", "apricots", "basmati rice", "cilantro",],
+        source: "Antoni Porowski"
+    },
+    {
+        name: "Shakshuka",
+        ingredients: ["eggs", "san marzano tomatoes", "cumin", "onion", "red pepper", "tumeric"],
+        source: "Eden Grinshpan",
+    },
+    {
+        name: "Red beans and rice ",
+        ingredients: ["canned kidney beans", "bay leaf", "cayenne pepper", "black pepper", "celery", "bell pepper", "onion", "cajun season", "sausage", "rice", ],
+        source: "Savoy Family",
+    },
+    {
+        name: "Veggie Taco Salad",
+        ingredients: ["romaine lettuce", "walnuts", "cauliflower", "taco seasoning", "salsa", "avocado", "jalapeno", ],
+        source: "Pinch of Yum",
+    },
+    {
+        name: "Salmon Teriyaki bowls ",
+        ingredients: ["salmon", "teriyaki sauce", "sushi rice", "brocoli", "hot sesame oil", "shallots", "furiake", ],
+        source: "Khavoy",
+    },
+    {
+        name: "Korean BBQ bowls",
+        ingredients: ["prime rib", "sushi rice", "egg", "kale slaw", "mirin", "rice vinegar", "soy sauce", "ginger", "pear", ],
+        source: "Pinch of Yum",
+    },
+    {
+        name: "Tumeric cauliflower",
+        ingredients: ["Cauliflower florets", "tumeric", "gojuchang", "lime", "cilantro", "olive oil" "basmati rice", ],
+        source: "Antoni Porowski",
+    },
+    {
+        name: "Veggie Shawarma Pita  ",
+        ingredients: ["red peppers", "red onion", "chickpeas", "garlic", "shawarma seasoning", "pita bread", "grape tomatos", "parsley", "tahini", ],
+        source: "Pinch of Yum",
+    },
+    {
+        name: "Sweet potato/spinach/ egg bowl ",
+        ingredients: ["sweet potato", "spinach", "quinoa", "avocado", "red chilis", "tahini ", ],
+        source: "Pinch of Yum",
+    },
+    {
+        name: "Peas and Rice ",
+        ingredients: ["Frozen peas", "ground beef", "roma tomatoes", "onion", "cumin", "all spice", "sumac", "paprika", "salt", "basmati rice", ],
+        source: "Savoy Family",
+    },
+    {
+        name: "Turkey kefte and rice pilaf ",
+        ingredients: ["Ground Turkey", "Mint", "olive oil ", "onion", "cumin", "aleppo peppers", "cinnamon", "pepper", "rice", "grape tomatoes", "red onion", ],
+        source: "Antoni Porowski",
+    },
+    {
+        name: "Strawberry Salad",
+        ingredients: ["strawberries", "chickpeas", "mixed lettuce", "red onion", "avocado", "tahini", "mustard", "balsamic", "olive oil ", ],
+        source: "Khavoy",
+    },
+    {
+        name: "Avocado Toast",
+        ingredients: ["bread", "avocado", "lime", "salt" ],
+        source: "Khavoy",
+    },
+    {
+        name: "Madeira",
+        ingredients: ["onion", "black lentils", "basmatie rice", "cumin", "tumeric", "tahini", "lemon", "grape tomatoes", "olive oil", "grapeseed oil", "shallots",],
+        source: "Eden Grinsphan",
+    },
+    {
+        name: "Tamale casserole",
+        ingredients: ["canned black beans", "canned kidney beans", "canned corn", "masa", "butter", "veggie broth", "chili seasoning", "cumin", "jalapeno", ],
+        source: "Khavoy",
+    },
+    {
+        name: "Asparagus, tomato pesto pasta",
+        ingredients: ["asparagus", "pesto", "grape tomatoes", "bow tie Noodles", ],
+        source: "Khavoy",
+    },
+    {
+        name: "Japanese egg and rice with spicy broccoli",
+        ingredients: ["sushi rice", "egg", "brocoli", "furiake", "soy sauce", "sesame oil", ],
+        source: "Breath of the Wild",
+    },
+    {   name: "Tomato Toast", 
+        ingredients: ["tomato", "bread", "cream cheese", "balsamic"],
+        source: "Khavoy", 
+    },
+],
+
